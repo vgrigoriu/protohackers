23efc6e32dc8ba9e02a81a5c79292c9b5cc834f0
Victor Grigoriu <vgrigoriu@gmail.com>
2022-12-23 06:55:57 +0200

    Change main to run multiple services

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,13 @@
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 given ExecutionContext = ExecutionContext.global
 
-@main def hello(): Unit =
-  runService(12345, Challenge0)
+@main def main(): Unit =
+  val services: List[ClientHandler] = List(Challenge0)
+  val futures = services.zipWithIndex.map((service, i) => Future {
+    runService(2000 + i, service)
+  })
+
+  // "Wait" for all services to "finish" (which they'll never will).
+  Await.result(Future.sequence(futures), Duration.Inf)
