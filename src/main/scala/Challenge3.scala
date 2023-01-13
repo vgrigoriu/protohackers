237b852c59ffcd949ee576a38634aaefe8fcf967
Victor Grigoriu <vgrigoriu@gmail.com>
2023-01-13 11:17:44 +0200

    Challenge3: pass all the tests

--- src/main/scala/Challenge3.scala
+++ src/main/scala/Challenge3.scala
@@ -1,4 +1,5 @@
 import java.io.{BufferedReader, InputStream, InputStreamReader, OutputStream}
+import java.net.{Socket, SocketException}
 import java.nio.charset.StandardCharsets.UTF_8
 import java.util.concurrent.Executors
 import scala.collection.mutable
@@ -23,6 +24,7 @@ class User(input: InputStream, output: OutputStream, messageHandler: (User, Stri
 object Chat:
   private val executor = Executors.newSingleThreadExecutor()
 
+  // TODO: Wrap these sets in thread-safe adapters.
   private val newUsers: mutable.Set[User] = mutable.Set()
   private val joinedUsers: mutable.Set[User] = mutable.Set()
 
@@ -38,17 +40,20 @@ object Chat:
   private def handleNewUser(user: User, message: String): Boolean =
     val name = message
     if !"[a-zA-Z0-9]+".r.matches(name) then
-      user.send(s"Invalid username: $name")
+      send(user, s"Invalid username: $name")
       println(s"Trying to set invalid username: name")
       false
     else
       if joinedUsers.isEmpty then
-        user.send("* You're the only one here.")
+        send(user, "* You're the only one here.")
       else
-        user.send(s"* Already here: ${joinedUsers.map(_.name).mkString(", ")}")
+        send(user, s"* Already here: ${joinedUsers.map(_.name).mkString(", ")}")
 
       println(s"New user name is $message")
-      // TODO: announce existing users.
+
+      val currentUsers = executor.submit(() => joinedUsers.clone()).get
+      currentUsers.foreach ( send(_, s"* $name has entered the room."))
+
       executor.submit(() =>
         newUsers.remove(user)
         user.name = name
@@ -57,13 +62,38 @@ object Chat:
       true
 
   private def handleJoinedUser(user: User, message: String): Boolean =
-    // TODO: send message to other users.
     println(s"Got message: [${user.name}] $message")
+    val currentUsers = executor.submit(() => joinedUsers.clone()).get
+    currentUsers.remove(user)
+    currentUsers.foreach(send(_, s"[${user.name}] $message"))
     true
+
+  private def send(user: User, message: String): Unit =
+    try
+      user.send(message)
+    catch
+      case e: SocketException =>
+        println(s"${user.name} connection closed.")
+        removeUser(user)
+
+  private def removeUser(user: User): Unit =
+    val shouldAnnounce = executor.submit(() => {
+      newUsers.remove(user)
+      // Only announce departure if user was in joinedUsers.
+      joinedUsers.remove(user)
+    }).get()
+
+    if shouldAnnounce then
+      val currentUsers = executor.submit(() => joinedUsers.clone()).get
+      currentUsers.foreach(send(_, s"* ${user.name} has left the room."))
 
   def addNewUser(input: InputStream, output: OutputStream): Unit =
     val newUser = User(input, output, handle)
-    executor.submit(() => newUsers.add(newUser)).get
     newUser.send("Welcome! How do you want us to call you?")
+    executor.submit(() => newUsers.add(newUser)).get
     newUser.receive()
+
+    // Finished receiving, remove user.
+    println(s"Finished receiving from ${newUser.name}")
+    removeUser(newUser)
 
